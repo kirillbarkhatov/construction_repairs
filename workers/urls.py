65a77f4f61eb53b25fd98d861b21774298a27f9c
kirill_barkhatov <k.s.barkhatov@gmail.com>
2024-12-27 21:30:26 +0300

    эндпоинт для списка выбранной бригады

--- workers/urls.py
+++ workers/urls.py
@@ -1,23 +1,12 @@
 from django.urls import path
 
 from .apps import WorkersConfig
-from .views import WorkerRetrieveAPIView
+from .views import WorkerRetrieveAPIView, WorkersTeamListApiView
 
 app_name = WorkersConfig.name
 
 
 urlpatterns = [
-    # path("api/v1/team/<team_id>/WorkerLis", LessonListApiView.as_view(), name="team_workers_list"),
+    path("api/v1/team/<team_id>/WorkerList", WorkersTeamListApiView.as_view(), name="team_workers_list"),
     path("api/v1/worker/<int:pk>", WorkerRetrieveAPIView.as_view(), name="worker_retrieve"),
-    # path("lesson/create", LessonCreateApiView.as_view(), name="lesson_create"),
-    # path("lesson/<int:pk>/update", LessonUpdateAPIView.as_view(), name="lesson_update"),
-    # path(
-    #     "lesson/<int:pk>/delete", LessonDestroyAPIView.as_view(), name="lesson_delete"
-    # ),
-    # path(
-    #     "course/subscribe", CourseSubscriptionApiView.as_view(), name="course_subscribe"
-    # ),
-    # path("payment/", CoursePaymentCreateApiView.as_view(), name="payment"),
 ]
-
-# urlpatterns += router.urls
--- workers/views.py
+++ workers/views.py
@@ -3,8 +3,21 @@ from rest_framework import generics
 from .models import Worker
 from .serializers import WorkerSerializer
 
+
 class WorkerRetrieveAPIView(generics.RetrieveAPIView):
     """Получить информацию о работнике"""
 
     serializer_class = WorkerSerializer
     queryset = Worker.objects.all()
+
+
+class WorkersTeamListApiView(generics.ListAPIView):
+    """Список работников бригады"""
+
+    serializer_class = WorkerSerializer
+
+    def get_queryset(self):
+        """Фильтруем набор данных в зависимости от выбранной бригады"""
+
+        team_id = self.kwargs.get("team_id")
+        return Worker.objects.filter(team=team_id)

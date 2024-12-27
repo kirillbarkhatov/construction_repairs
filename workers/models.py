e6464cc3c0ea5ab1fa2686ed87c345ad0d68594c
kirill_barkhatov <k.s.barkhatov@gmail.com>
2024-12-27 20:36:04 +0300

    Модель и миграции

--- workers/models.py
+++ workers/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Worker(models.Model):
+    """Модель работников"""
+
+    full_name = models.CharField(max_length=200, verbose_name="ФИО")
+    team = models.PositiveIntegerField(verbose_name="Номер бригады")
+    salary = models.PositiveIntegerField(verbose_name="Зарплата")
+    specialization = models.CharField(max_length=50, verbose_name="Специализация")
+
+    def __str__(self):
+        return f"{self.full_name} - Бригада № {self.team} - {self.specialization}"
+
+    class Meta:
+        verbose_name = "Работник"
+        verbose_name_plural = "Работники"
+        ordering = ["pk", ]
